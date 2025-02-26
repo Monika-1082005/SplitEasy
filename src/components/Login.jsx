1faa2f88e90bf4487c7359d3b3931f5f0197e1d5
Jyoti Gaud <23jyotigaud@gmail.com>
2025-02-26 11:17:00 +0530

    Add background and responsiveness to login-signup

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,31 +1,40 @@
 import loginImage from "../../src/assets/login.jpeg";
+
 import { Link } from "react-router-dom";
 
 export default function Login() {
   return (
-    <section className="min-h-screen flex items-center justify-center font-mono bg-white">
-      <div className="flex flex-col xl:flex-row shadow-2xl bg-white rounded-2xl overflow-hidden max-w-[900px] w-full">
+    <section className="min-h-screen flex items-center justify-center bg-[url('../../src/assets/bg.png')] bg-cover">
+      <div className="flex shadow-2xl">
         {/* Left Side - Login Form */}
-        <div className="flex flex-col items-center justify-center text-center px-12 py-16 gap-6 bg-[#1F3C9A] text-white flex-1 max-w-[450px] w-full">
+        <div className="flex flex-col items-center justify-center text-center w-[300px] px-8 py-7 sm:w-[450px] sm:p-14 gap-6 bg-[#1F3C9A] rounded-2xl text-white lg:rounded-tr-none lg:rounded-br-none lg:p-16">
           <h1 className="text-4xl font-bold text-[#F3E9D3]">Welcome</h1>
-          <form className="flex flex-col text-lg text-left gap-4 w-full">
-            <label className="text-lg text-[#F3E9D3]">Phone No.</label>
+          <form className="flex flex-col text-lg text-left w-full">
+            <label className="text-lg text-[#F3E9D3] mb-1 mt-4">
+              Phone No.
+            </label>
             <input
-              type="text"
+              type="tel"
+              id="phone"
+              name="phone"
+              pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
               className="p-2 border border-gray-300 rounded-lg placeholder-gray-400 focus:border-[#9DC3ED] focus:bg-white text-[#1d214b] focus:outline-none"
               placeholder="Enter your phone no."
             />
-            <label className="text-lg text-[#F3E9D3]">OTP</label>
+            <label className="text-lg text-[#F3E9D3] mb-1 mt-4">OTP</label>
             <input
-              type="password"
+              type="text"
+              inputMode="numeric"
+              id="otp"
+              name="otp"
               className="p-2 border border-gray-300 rounded-lg placeholder-gray-400 focus:border-[#9DC3ED] focus:bg-white focus:text-[#1d214b] focus:outline-none"
               placeholder="******"
             />
-            <div className="flex gap-2 items-center">
-              <input type="checkbox" className="accent-[#9DC3ED]" />
-              <span className="text-sm">Remember me</span>
+            <div className="flex gap-2 items-center mb-1 mt-4">
+              <input type="checkbox" className="accent-[#9DC3ED] " />
+              <span className="text-sm ">Remember me</span>
             </div>
-            <button className="px-10 py-3 text-lg rounded-lg bg-[#1D214B] text-white hover:bg-[#9DC3ED] hover:text-[#1d214b] transition">
+            <button className="mb-1 mt-4 px-10 py-3 text-lg rounded-lg bg-[#1D214B] text-white hover:bg-[#9DC3ED] hover:text-[#1d214b] transition">
               Login
             </button>
           </form>
@@ -39,13 +48,12 @@ export default function Login() {
             </Link>
           </div>
         </div>
-        <div className="hidden xl:flex flex-1 items-center justify-center overflow-hidden">
-          <img
-            src={loginImage}
-            alt="Login"
-            className="w-full h-full object-contain rounded-tr-2xl rounded-br-2xl"
-          />
-        </div>
+
+        <img
+          src={loginImage}
+          alt="Login"
+          className="w-[480px] object-cover lg:rounded-tr-2xl lg:rounded-br-2xl lg:block hidden"
+        />
       </div>
     </section>
   );
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -3,36 +3,48 @@ import { Link } from "react-router-dom";
 
 export default function Signup() {
   return (
-    <section className="min-h-screen flex items-center justify-center font-mono bg-gradient-to-r from-[#9DC3ED] via-[#F3E9D3] to-[#9DC3ED]">
-      <div className="flex flex-col xl:flex-row shadow-2xl bg-white rounded-2xl overflow-hidden max-w-[900px] w-full">
-        {/* Left Side - Login Form */}
-        <div className="flex flex-col items-center justify-center text-center px-12 py-16 gap-6 bg-[#1F3C9A] text-white flex-1 max-w-[450px] w-full">
-          <h1 className="text-4xl font-bold text-[#F3E9D3]">Sign Up</h1>
-          <form className="flex flex-col text-lg text-left gap-4 w-full">
-            <label className="text-lg text-[#F3E9D3]">Username</label>
+    <section className="min-h-screen flex items-center justify-center bg-[url('../../src/assets/bg.png')] bg-cover">
+      <div className="flex shadow-2xl">
+        {/* Left Side - Sign Up Form */}
+        <div className="flex flex-col items-center justify-center text-center w-[300px] px-8 py-7 sm:w-[450px] sm:p-14 gap-0 sm:gap-6 bg-[#1F3C9A] rounded-2xl text-white lg:rounded-tr-none lg:rounded-br-none lg:p-16">
+          <h1 className=" text-2xl sm:text-4xl font-bold text-[#F3E9D3]">
+            Sign Up
+          </h1>
+          <form className="flex flex-col text-lg text-left w-full">
+            <label className="text-lg text-[#F3E9D3] mb-1 mt-4">Username</label>
             <input
               type="text"
+              id="username"
+              name="username"
               className="p-2 border border-gray-300 rounded-lg placeholder-gray-400 focus:border-[#9DC3ED] focus:bg-white text-[#1d214b] focus:outline-none"
               placeholder="Enter your username"
             />
-            <label className="text-lg text-[#F3E9D3]">Phone No.</label>
+            <label className="text-lg text-[#F3E9D3] mb-1 mt-4">
+              Phone No.
+            </label>
             <input
-              type="text"
+              type="tel"
+              id="phone"
+              name="phone"
+              pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
               className="p-2 border border-gray-300 rounded-lg placeholder-gray-400 focus:border-[#9DC3ED] focus:bg-white text-[#1d214b] focus:outline-none"
               placeholder="Enter your phone no."
             />
-            <label className="text-lg text-[#F3E9D3]">OTP</label>
+            <label className="text-lg text-[#F3E9D3] mb-1 mt-4">OTP</label>
             <input
-              type="password"
+              type="text"
+              inputMode="numeric"
+              id="otp"
+              name="otp"
               className="p-2 border border-gray-300 rounded-lg placeholder-gray-400 focus:border-[#9DC3ED] focus:bg-white focus:text-[#1d214b] focus:outline-none"
               placeholder="******"
             />
-            <div className="flex gap-2 items-center">
+            <div className="flex mt-4 items-center">
               <input type="checkbox" className="accent-[#9DC3ED]" />
               <span className="text-sm">Remember me</span>
             </div>
-            <button className="px-10 py-3 text-lg rounded-lg bg-[#1D214B] text-white hover:bg-[#9DC3ED] hover:text-[#1d214b] transition">
-              Login
+            <button className="px-6 py-3 mt-4 mb-4 text-lg rounded-lg bg-[#1D214B] text-white hover:bg-[#9DC3ED] hover:text-[#1d214b] transition">
+              Sign Up
             </button>
           </form>
           <div className="flex flex-col items-center gap-0">
@@ -41,18 +53,17 @@ export default function Signup() {
               to="/login"
               className="text-sm text-[#9DC3ED] hover:underline"
             >
-              Sign In
+              Login
             </Link>
           </div>
         </div>
+
         {/* Right Side - Image (Hidden on Small Screens) */}
-        <div className="hidden xl:flex flex-1 items-center justify-center overflow-hidden">
-          <img
-            src={signUpImage}
-            alt="Login"
-            className="w-full h-full object-contain rounded-tr-2xl rounded-br-2xl"
-          />
-        </div>
+        <img
+          src={signUpImage}
+          alt="Sign Up"
+          className="w-[490px] lg:rounded-tr-2xl lg:rounded-br-2xl lg:block hidden"
+        />
       </div>
     </section>
   );
