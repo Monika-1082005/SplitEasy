f1300132c3874bb957341192e3e72ecd3fc3fd19
Monika <dalawatmonika2801@gmail.com>
2025-04-28 12:02:38 +0530

    Created separate folder for client and server hence removed files from their original location

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-[#1d214b] text-white">
-      <div className="flex text-sm items-center justify-center p-4">
-        Created By Jyoti and Monika | Copyrights Reserved 2025&copy;
-      </div>
-    </footer>
-  );
-}
