a23c5adce11e7146409a9f31ff796adc52d90bed
Monika <dalawatmonika2801@gmail.com>
2025-09-29 20:54:30 +0530

    Added proptypes to setLoggedIn

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -4,6 +4,8 @@ import { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
+import PropTypes from "prop-types";
+
 
 export default function Login({ setLoggedIn }) {
   const [email, setEmail] = useState("");
@@ -164,3 +166,8 @@ export default function Login({ setLoggedIn }) {
     </section>
   );
 }
+
+Login.propTypes = {
+  setLoggedIn: PropTypes.func.isRequired,
+};
+
